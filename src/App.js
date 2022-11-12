7450bc09d4889e6a86e2fd223ba7f3837e9257a9
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2022-11-12 18:00:26 +0700

    create box add todo list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import TodoList from "./components/TodoList";
+import Textfield from '@atlaskit/textfield';
+import Button from '@atlaskit/button';
+
 
 function App() {
   return (
-    <TodoList />
+    <>
+      <h3>Danh sách cần làm</h3>
+      <Textfield name = 'add-todo' placeholder="Nhập công việc cần làm..." elemAfterInput = {
+        <Button isDisabled = {true} appearance="primary">Thêm</Button>
+      }
+      css ={{padding: "2px 4px 2px"}}
+      ></Textfield>
+      <TodoList />
+    </>
   );
 }
 
